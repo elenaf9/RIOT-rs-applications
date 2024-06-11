6422a02b38c7d34537adaa7decd78d8c7088dfbf
Elena Frank <elena.frank@protonmail.com>
2024-06-11 16:04:50 +0200

    benchmarks: add `bench_sched`

---
+++ benchmarks/bench_sched/src/main.rs
@@ -0,0 +1,19 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+#![feature(used_with_arg)]
+
+use riot_rs::{debug::println, thread};
+
+#[riot_rs::thread(autostart)]
+fn thread0() {
+    match riot_rs::bench::benchmark(10000, || thread::schedule()) {
+        Ok(ticks) => 
+            println!(
+                "took {} ticks per iteration", ticks),
+        
+        Err(_) =>
+            println!("benchmark returned error"),
+        
+    }
+}
