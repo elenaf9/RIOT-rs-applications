c8d65f406032b37d20da2998b478436068407037
Elena Frank <elena.frank@protonmail.com>
2024-06-11 16:03:20 +0200

    benchmarks: add `bench_runqueue_get_next`

---
+++ benchmarks/bench_runqueue_get_next/README.md
@@ -0,0 +1,11 @@
+# bench_runqueue_get_next
+
+## About
+
+This benchmark tests getting the next thread from the runqueue.
+
+## How to run
+
+In this folder, run
+
+    laze build -b rpi-pico run
---
+++ benchmarks/bench_runqueue_get_next/src/main.rs
@@ -0,0 +1,24 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+#![feature(used_with_arg)]
+
+use riot_rs::{
+    debug::println,
+    thread::{SCHED_PRIO_LEVELS, THREADS_NUMOF},
+};
+use riot_rs_runqueue::{RunQueue, RunqueueId, ThreadId};
+
+#[riot_rs::thread(autostart)]
+fn thread0() {
+    let mut rq = RunQueue::<{ SCHED_PRIO_LEVELS }, { THREADS_NUMOF }>::new();
+    rq.add(ThreadId::new(0), RunqueueId::new(5));
+    match riot_rs::bench::benchmark(10000, || {
+        let next = rq.get_next();
+        core::hint::black_box(next);
+        core::hint::black_box(&mut rq);
+    }) {
+        Ok(ticks) => println!("took {} ticks per iteration ", ticks),
+        Err(_) => println!("benchmark returned error"),
+    }
+}
