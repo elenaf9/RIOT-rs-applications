a5b5bec80110065e44330440b6098457e37638ce
Elena Frank <elena.frank@protonmail.com>
2024-06-17 10:34:10 +0200

    benchmarks: add `bench_critical_section`

---
+++ benchmarks/bench_critical_section/src/main.rs
@@ -0,0 +1,31 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+#![feature(used_with_arg)]
+
+use riot_rs::{debug::println, thread};
+
+#[riot_rs::thread(autostart)]
+fn thread0() {
+    match riot_rs::bench::benchmark(10_000, || {
+        thread::current_pid().unwrap();
+    }) {
+        Ok(ticks) => println!("took {} ticks per iteration", ticks),
+        Err(_) => println!("benchmark returned error"),
+    }
+    loop {}
+}
+
+#[riot_rs::thread(autostart)]
+fn thread1() {
+    loop {
+        critical_section::with(|_| {
+            // Some dummy computation.
+            let mut counter = 0;
+            for _ in 0..1_000 {
+                counter = core::hint::black_box(counter + 1);
+            }
+            core::hint::black_box(counter);
+        })
+    }
+}
