b1e1ef210e6a7392f6f903c4332ffb4fcb440aab
Elena Frank <elena.frank@protonmail.com>
2024-06-24 18:29:35 +0200

    benchmarks: add `bench_runqueue_reallocate\'

---
+++ benchmarks/runqueue/bench_runqueue_reallocate/README.md
@@ -0,0 +1,11 @@
+# bench_runqueue_get_next
+
+## About
+
+This benchmark tests the reallocation computation for multicore-v1.
+
+## How to run
+
+In this folder, run
+
+    laze build -b rpi-pico run
---
+++ benchmarks/runqueue/bench_runqueue_reallocate/src/main.rs
@@ -0,0 +1,32 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+#![feature(used_with_arg)]
+
+use riot_rs::{
+    debug::println,
+    thread::{SCHED_PRIO_LEVELS, THREADS_NUMOF},
+};
+use riot_rs_runqueue::{GlobalRunqueue, RunQueue, RunqueueId, ThreadId};
+
+#[riot_rs::thread(autostart)]
+fn thread0() {
+    let mut rq = RunQueue::<{ SCHED_PRIO_LEVELS }, { THREADS_NUMOF }>::new();
+    rq.add(ThreadId::new(0), RunqueueId::new(5));
+    rq.add(ThreadId::new(1), RunqueueId::new(4));
+    match riot_rs::bench::benchmark(10000, || {
+        let changed_core = rq.reallocate();
+        core::hint::black_box(changed_core);
+        core::hint::black_box(&mut rq);
+    }) {
+        Ok(ticks) => println!("took {} ticks per iteration ", ticks),
+        Err(_) => println!("benchmark returned error"),
+    }
+    loop {}
+}
+
+#[cfg(feature = "multicore")]
+#[riot_rs::thread(autostart)]
+fn thread1() {
+    cortex_m::asm::wfi();
+}
