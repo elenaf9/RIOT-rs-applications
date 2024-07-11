c1fc050b594748c017123088dd728a47677999a9
Elena Frank <elena.frank@protonmail.com>
2024-07-11 15:36:34 +0200

    bench_async: ensure benchmark runs on Core0

--- benchmarks/bench_async/src/main.rs
+++ benchmarks/bench_async/src/main.rs
@@ -4,6 +4,8 @@
 #![feature(used_with_arg)]
 
 use embassy_time::{Duration, Timer};
+#[cfg(feature = "multicore")]
+use riot_rs::thread::CoreId;
 use riot_rs::{
     debug::log::*,
     embassy::thread_executor::Executor,
@@ -15,22 +17,31 @@ const ITERATIONS: usize = 100;
 
 #[riot_rs::task(autostart)]
 async fn start_other_tasks() {
-    riot_rs::thread::thread_flags::set(ThreadId::new(1), 0b110);
+    thread_flags::set(ThreadId::new(0), 0b1);
+    thread_flags::set(ThreadId::new(1), 0b110);
     #[cfg(feature = "multicore")]
-    riot_rs::thread::thread_flags::set(ThreadId::new(2), 0b110);
+    thread_flags::set(ThreadId::new(2), 0b110);
 }
 
 #[riot_rs::task(pool_size = 2)]
 async fn task(id: usize) {
-    riot_rs::thread::thread_flags::wait_one(0b110);
+    thread_flags::wait_one(0b110);
     for _ in 0..ITERATIONS {
         Timer::after(Duration::from_millis(1)).await;
     }
     thread_flags::set(ThreadId::new(0), 1 << id);
+
+    /// Blocks Core 1 so that the benchmark has to continue running on Core 0;
+    /// FIXME: implement core affinity masks instead.
+    #[cfg(feature = "multicore")]
+    if riot_rs::thread::core_id() == CoreId::new(1) {
+        loop {}
+    }
 }
 
-#[riot_rs::thread(autostart, priority = 2)]
+#[riot_rs::thread(autostart, priority = 1)]
 fn thread0() {
+    thread_flags::wait_one(0b1);
     match riot_rs::bench::benchmark(1, || {
         thread_flags::wait_all(0b11);
     }) {
