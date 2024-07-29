41312c92d5f216582494792081dec940400d4b2f
Elena Frank <elena.frank@protonmail.com>
2024-07-29 12:14:35 +0200

    bench_sched_yield: test different thread counts

--- benchmarks/bench_sched_yield/src/main.rs
+++ benchmarks/bench_sched_yield/src/main.rs
@@ -7,18 +7,31 @@ use riot_rs::{debug::log::*, thread};
 
 #[riot_rs::thread(autostart)]
 fn thread0() {
-    match riot_rs::bench::benchmark(10000, || thread::yield_same()) {
-        Ok(ticks) => info!(
-            "took {} ticks per iteration ({} per context switch)",
-            ticks,
-            ticks / 2
-        ),
+    match riot_rs::bench::benchmark(1000, || thread::yield_same()) {
+        Ok(ticks) => info!("took {} ticks per iteration", ticks,),
         Err(_) => error!("benchmark returned error"),
     }
 }
 
+#[cfg(not(feature = "t1"))]
 #[riot_rs::thread(autostart)]
 fn thread1() {
+    loop {
+        thread::yield_same()
+    }
+}
+
+#[cfg(any(feature = "t3", feature = "t4"))]
+#[riot_rs::thread(autostart)]
+fn thread2() {
+    loop {
+        thread::yield_same()
+    }
+}
+
+#[cfg(feature = "t4")]
+#[riot_rs::thread(autostart)]
+fn thread3() {
     loop {
         thread::yield_same()
     }
