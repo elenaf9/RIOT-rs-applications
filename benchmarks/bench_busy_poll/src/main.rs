e18cd44340a16a8ac24bbfec3439479b4db2b1c9
Elena Frank <elena.frank@protonmail.com>
2024-07-29 10:01:34 +0200

    bench_busy_poll: remove network task
    Only run the critical task, so that the difference between busy polling
    and awaiting can be micro-benchmarked without obfuscation by the
    parallel network task.

--- benchmarks/bench_busy_poll/src/main.rs
+++ benchmarks/bench_busy_poll/src/main.rs
@@ -3,7 +3,9 @@
 #![feature(type_alias_impl_trait)]
 #![feature(used_with_arg)]
 
-use embassy_time::{Duration, Timer};
+use embassy_time::Duration;
+#[cfg(feature = "await")]
+use embassy_time::Timer;
 use riot_rs::{
     debug::log::*,
     embassy::thread_executor::Executor,
@@ -15,30 +17,13 @@ const ITERATIONS: usize = 100;
 
 #[riot_rs::task(autostart)]
 async fn start_other_tasks() {
-    thread_flags::set(ThreadId::new(0), 0b10);
     thread_flags::set(ThreadId::new(1), 0b10);
-    thread_flags::set(ThreadId::new(2), 0b10);
+    thread_flags::set(ThreadId::new(0), 0b10);
 }
 
 #[cfg(feature = "poll")]
 fn now() -> u64 {
     embassy_time_driver::now()
-}
-
-#[riot_rs::task(pool_size = 1)]
-async fn network_task() {
-    thread_flags::wait_one(0b10);
-    let delay = Duration::from_millis(10);
-    loop {
-        Timer::after(delay).await;
-
-        // Some dummy computation.
-        let mut counter = 0;
-        for _ in 0..10_000 {
-            counter = core::hint::black_box(counter + 1);
-        }
-        core::hint::black_box(counter);
-    }
 }
 
 #[riot_rs::task(pool_size = 1)]
@@ -59,7 +44,7 @@ async fn critical_task() {
     thread_flags::set(ThreadId::new(0), 1);
 }
 
-#[riot_rs::thread(autostart, priority = 3)]
+#[riot_rs::thread(autostart)]
 fn thread0() {
     thread_flags::wait_one(0b10);
     match riot_rs::bench::benchmark(1, || {
@@ -71,18 +56,8 @@ fn thread0() {
     }
 }
 
-// This thread has a higher priority than the `thread2` because
-// otherwise it would be blocked by the busy poll in `critical_task`.
-#[riot_rs::thread(autostart, priority = 2, stacksize = 4096)]
+#[riot_rs::thread(autostart, stacksize = 4096)]
 fn thread1() {
-    let executor = make_static!(Executor::new());
-    executor.run(|spawner| {
-        spawner.must_spawn(network_task());
-    });
-}
-
-#[riot_rs::thread(autostart, priority = 1, stacksize = 4096)]
-fn thread2() {
     let executor = make_static!(Executor::new());
     executor.run(|spawner| {
         spawner.must_spawn(critical_task());
