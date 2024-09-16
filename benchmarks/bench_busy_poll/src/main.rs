f840294e346f7b0f87f2595ad4a7c82f0cf30fb3
Elena Frank <elena.frank@protonmail.com>
2024-09-16 16:43:22 +0200

    bench_busy_poll: simplify

--- benchmarks/bench_busy_poll/src/main.rs
+++ benchmarks/bench_busy_poll/src/main.rs
@@ -1,5 +1,6 @@
 #![no_main]
 #![no_std]
+#![feature(impl_trait_in_assoc_type)]
 #![feature(type_alias_impl_trait)]
 #![feature(used_with_arg)]
 
@@ -8,9 +9,7 @@ use embassy_time::Duration;
 use embassy_time::Timer;
 use riot_rs::{
     debug::log::*,
-    static_cell::make_static,
     thread::{thread_flags, ThreadId},
-    thread_executor::Executor,
 };
 
 #[cfg(feature = "affinity")]
@@ -18,21 +17,17 @@ use riot_rs::thread::{CoreAffinity, CoreId};
 
 const ITERATIONS: usize = 100;
 
-#[riot_rs::task(autostart)]
-async fn start_other_tasks() {
-    thread_flags::set(ThreadId::new(1), 0b10);
-    thread_flags::set(ThreadId::new(0), 0b10);
-}
-
 #[cfg(feature = "poll")]
 fn now() -> u64 {
     embassy_time_driver::now()
 }
 
-#[riot_rs::task(pool_size = 1)]
+#[riot_rs::task(autostart)]
 async fn critical_task() {
     let delay = Duration::from_millis(1);
 
+    // Start benchmark.
+    thread_flags::set(ThreadId::new(0), 0b10);
     for _ in 0..ITERATIONS {
         #[cfg(feature = "await")]
         Timer::after(delay).await;
@@ -43,27 +38,16 @@ async fn critical_task() {
             while now() < expires {}
         }
     }
-    thread_flags::set(ThreadId::new(0), 1);
+    thread_flags::set(ThreadId::new(0), 0b10);
 }
 
-#[cfg_attr(not(feature = "affinity"), riot_rs::thread(autostart))]
-#[cfg_attr(feature = "affinity", riot_rs::thread(autostart, affinity = CoreAffinity::one(CoreId::new(0))))]
+#[riot_rs::thread(autostart)]
 fn thread0() {
-    thread_flags::wait_one(0b10);
-    thread_flags::wait_all(1);
+    thread_flags::wait_all(0b10);
     match riot_rs::bench::benchmark(1, || {
-        thread_flags::wait_all(1);
+        thread_flags::wait_all(0b10);
     }) {
         Ok(ticks) => info!("took {} ticks", ticks),
-
         Err(_) => error!("benchmark returned error"),
     }
-}
-
-#[riot_rs::thread(autostart, stacksize = 4096)]
-fn thread1() {
-    let executor = make_static!(Executor::new());
-    executor.run(|spawner| {
-        spawner.must_spawn(critical_task());
-    });
 }
