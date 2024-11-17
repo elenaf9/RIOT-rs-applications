f50d32ca57f7bf5c522cbe544cec1d458618e0a3
Elena Frank <elena.frank@protonmail.com>
2024-11-17 21:14:19 +0100

    bench_matrix_multi: also split computation for single-core

--- benchmarks/bench_matrix_mult/src/main.rs
+++ benchmarks/bench_matrix_mult/src/main.rs
@@ -3,7 +3,7 @@
 #![feature(type_alias_impl_trait)]
 #![feature(used_with_arg)]
 #![feature(impl_trait_in_assoc_type)]
-#![feature(split_array)]
+#![feature(array_chunks)]
 
 use core::usize;
 
@@ -12,9 +12,9 @@ use riot_rs::thread::sync::Channel;
 use riot_rs::{debug::log::*, thread};
 
 #[cfg(feature = "dual-core")]
-static INPUT_CHANNEL: Channel<([[u8; N]; N / 2], [[u8; N]; N])> = Channel::new();
+static INPUT_CHANNEL: Channel<([[u16; N]; N / 2], [[u16; N]; N])> = Channel::new();
 #[cfg(feature = "dual-core")]
-static RESULT_CHANNEL: Channel<[[u8; N]; N / 2]> = Channel::new();
+static RESULT_CHANNEL: Channel<[[u16; N]; N / 2]> = Channel::new();
 
 #[cfg(feature = "n10")]
 const N: usize = 10;
@@ -25,12 +25,8 @@ const N: usize = 30;
 #[cfg(feature = "n40")]
 const N: usize = 40;
 
-fn matrix_mult<const M: usize>(
-    matrix_a: &[[u8; N]; M],
-    matrix_b: &[[u8; N]; N],
-    matrix_c: &mut [[u8; N]; M],
-) {
-    for i in 0..M {
+fn matrix_mult(matrix_a: &[[u16; N]], matrix_b: &[[u16; N]], matrix_c: &mut [[u16; N]]) {
+    for i in 0..N / 2 {
         for j in 0..N {
             for k in 0..N {
                 matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
@@ -50,27 +46,31 @@ fn thread0() {
 
     let matrix_a = core::hint::black_box([[3; N]; N]);
     let matrix_b = core::hint::black_box([[7; N]; N]);
+
     match bench_multicore::benchmark(10, || {
         let mut matrix_c = core::hint::black_box([[0; N]; N]);
+
+        let mut matrix_a_iter = matrix_a.array_chunks::<{ N / 2 }>();
+        let mut matrix_c_iter = matrix_c.array_chunks_mut::<{ N / 2 }>();
+
+        let matrix_a1 = matrix_a_iter.next().unwrap();
+        let matrix_a2 = matrix_a_iter.next().unwrap();
+
+        #[cfg(feature = "dual-core")]
+        INPUT_CHANNEL.send(&(*matrix_a2, matrix_b));
+
+        matrix_mult(matrix_a1, &matrix_b, matrix_c_iter.next().unwrap());
+
         #[cfg(not(feature = "dual-core"))]
         {
-            matrix_mult(&matrix_a, &matrix_b, &mut matrix_c);
+            matrix_mult(matrix_a2, &matrix_b, matrix_c_iter.next().unwrap());
         }
+
         #[cfg(feature = "dual-core")]
         {
-            let (matrix_a1, matrix_a2) = matrix_a.split_at(N / 2);
-            let matrix_a1: [_; N / 2] = matrix_a1.try_into().unwrap();
-            let matrix_a2: [_; N / 2] = matrix_a2.try_into().unwrap();
-
-            INPUT_CHANNEL.send(&(matrix_a2, matrix_b));
-
-            matrix_mult(&matrix_a1, &matrix_b, matrix_c.split_array_mut().0);
-
-            let matrix_c2 = RESULT_CHANNEL.recv();
-            for i in 0..N/2 {
-                matrix_c[N/2 + i] = matrix_c2[i];
-            }
+            *matrix_c_iter.next().unwrap() = RESULT_CHANNEL.recv();
         }
+
         core::hint::black_box(matrix_c);
     }) {
         Ok(ticks) => info!("took {} ticks per iteration", ticks),
