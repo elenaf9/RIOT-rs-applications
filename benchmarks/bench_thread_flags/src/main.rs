313ebf2130e257f9a27cd6463753c504778ab10a
Elena Frank <elena.frank@protonmail.com>
2024-11-17 21:13:02 +0100

    *: couple of fixes

--- benchmarks/bench_thread_flags/src/main.rs
+++ benchmarks/bench_thread_flags/src/main.rs
@@ -4,8 +4,6 @@
 #![feature(used_with_arg)]
 #![feature(impl_trait_in_assoc_type)]
 
-#[cfg(feature = "affinity")]
-use riot_rs::thread::{CoreAffinity, CoreId};
 use riot_rs::{
     debug::log::*,
     thread::{thread_flags, ThreadId},
@@ -13,20 +11,17 @@ use riot_rs::{
 
 #[riot_rs::task(autostart)]
 async fn start() {
-    thread_flags::set(ThreadId::new(1), 0b10);
-    thread_flags::set(ThreadId::new(0), 0b10);
+    thread_flags::set(ThreadId::new(0), 1);
+    thread_flags::set(ThreadId::new(1), 1);
 }
 
-#[cfg_attr(
-    not(feature = "affinity"),
-    riot_rs::thread(autostart, stacksize = 4094)
-)]
-#[cfg_attr(feature = "affinity", riot_rs::thread(autostart, stacksize = 4094, affinity = CoreAffinity::one(CoreId::new(0))))]
+#[riot_rs::thread(autostart)]
 fn thread0() {
-    while thread_flags::get() != 0b10 { }
+    thread_flags::wait_all(1);
+    thread_flags::set(ThreadId::new(0), 1);
     match bench_multicore::benchmark(1000, || {
-        thread_flags::set(ThreadId::new(2), 1);
         thread_flags::wait_all(1);
+        thread_flags::set(ThreadId::new(2), 1);
     }) {
         Ok(ticks) => info!("took {} ticks per iteration", ticks),
         Err(err) => error!("benchmark error: {}", err),
@@ -36,10 +31,9 @@ fn thread0() {
 
 #[riot_rs::thread(autostart)]
 fn thread1() {
-    thread_flags::wait_all(0b10);
     loop {
-        thread_flags::set(ThreadId::new(3), 1);
         thread_flags::wait_all(1);
+        thread_flags::set(ThreadId::new(3), 1);
     }
 }
 
